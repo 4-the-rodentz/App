eb082ea4362d687b29b67be946474086a9c8e503
Elisabeth Fillmore <Elisabeth Fillmore>
2016-08-14 11:43:45 -0700

    created 2 functions to create data for canvasjs to use

---
+++ javascript/chart.js
@@ -0,0 +1,51 @@
+var beaverCount = 0;
+var ratCount = 0;
+var voleCount = 0;
+var mouseCount = 0;
+
+function animalCounts() {
+for (var i = 0; i < locations.length; i++) {
+  if (locations[i].rodentType == "Beaver") {
+    beaverCount++
+  }
+  if (locations[i].rodentType == "Rat") {
+    ratCount++
+  }
+  if (locations[i].rodentType == "Vole") {
+    voleCount++
+  }
+  if (locations[i].rodentType == "Mouse") {
+    mouseCount++
+  }
+}
+}
+
+var animalsArray = [beaverCount, ratCount, voleCount, mouseCount];
+var newArr = [];
+
+function convert() {
+  newArr = [];
+  for (var i = 0; i < animalsArray.length; i++) {
+    var newObj = {
+      label: locations[i].rodentType,
+      y: animalsArray[i],
+    }
+
+    newArr.push(newObj);
+  }
+}
+
+function drawChart() {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		title:{
+			text: "Number of Rodents in the Greater Portland Area"
+		},
+		data: [
+		{
+			type: "column",
+			dataPoints: newArr
+		}
+		]
+	});
+	chart.render();
+}
--- javascript/formObjConst.js
+++ javascript/formObjConst.js
@@ -25,5 +25,13 @@ var DataPoint = function(userName, location, rodentType, size, date, lat, lng, w
 
 var locations = [
     new DataPoint('Jon', (45.531605383770994, -122.66063690185547, 10), "Beaver", "jan 1"),
-    new DataPoint('Dave', (45.531605383770994, -122.66063690185547, 10), "Beaver", "feb 10")
+    new DataPoint('Dave', (45.531605383770994, -122.66063690185547, 10), "Rat", "feb 10"),
+    new DataPoint('Karen', (45.51999283770994, -122.67762483770994, 10), "Vole", "jan 1"),
+    new DataPoint('Susan', (45.51189983770994, -122.62550383770994, 10), "Mouse", "feb 10"),
+    new DataPoint('Jane', (45.51442183770994, -122.63226283770994, 10), "Vole", "jan 1"),
+    new DataPoint('Maddie', (45.52249983770994, -122.63934383770994, 10), "Rat", "feb 10"),
+    new DataPoint('Carrie', (45.52249983770994, -122.63934383770994, 10), "Mouse", "jan 1"),
+    new DataPoint('Elisabeth', (45.52249983770994, -122.63934383770994, 10), "Mouse", "feb 10"),
+    new DataPoint('Caroline', (45.52200783770994, -122.69144283770994, 10), "Beaver", "jan 1"),
+    new DataPoint('Amy', (45.52123383770994, -122.69546583770994, 10), "Beaver", "feb 10")
 ];
