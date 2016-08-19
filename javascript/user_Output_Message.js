ed2f88eb8f233975f8f1bc3ea90ba3088eb6dac0
DrewStock <andrewdstock@gmail.com>
2016-08-18 20:24:46 -0700

    finalized adding conditional statement to user_Output_Message.js

--- javascript/user_Output_Message.js
+++ javascript/user_Output_Message.js
@@ -2,15 +2,15 @@ var getUserInfo = JSON.parse(localStorage.getItem("userData"));
 
 // Function to output message to user after submitting rodent submission form
 function userOutput() {
-  if (getUserInfo = null) {
+  if (getUserInfo != null) {
+    var userOutputMessage = document.getElementById("userOutputMessage");
+    userOutputMessage.setAttribute("class", "userOutputMessage-transition");
+    userOutputMessage.innerHTML = "Good job " + getUserInfo.userName + "! Your sighting of a " + getUserInfo.rodentType + " has been logged.";
+    console.log("Rodent submission log - userName: " + getUserInfo.userName + " has submitted a sighting of a: " + getUserInfo.rodentType);
+  } else {
     var userOutputMessage = document.getElementById("userOutputMessage");
     userOutputMessage.setAttribute("class", "userOutputMessage-transition");
     userOutputMessage.innerHTML = "Rodent sightings in PDX";
-  } else {
-  var userOutputMessage = document.getElementById("userOutputMessage");
-  userOutputMessage.setAttribute("class", "userOutputMessage-transition");
-  userOutputMessage.innerHTML = "Good job " + getUserInfo.userName + "! Your sighting of a " + getUserInfo.rodentType + " has been logged.";
-  console.log("Rodent submission log - userName: " + getUserInfo.userName + " has submitted a sighting of a: " + getUserInfo.rodentType);
   }
 };
 
